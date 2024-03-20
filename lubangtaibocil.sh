37696d87dad36a41aaed4cf8455d6afa0cba79d7
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-03-20 13:33:04 +0700

    Update and rename lubangtaibocil.php to lubangtaibocil.sh

--- lubangtaibocil.sh
+++ lubangtaibocil.sh
@@ -1,12 +1,12 @@
-<?php
+#!/usr/bin/bash
+
 echo "    __ __          _          ______                
    / //_/__ _   __(_)___     / ____/___ _____  _____
   / ,< / _ \ | / / / __ \   / / __/ __ `/ __ \/ ___/
  / /| /  __/ |/ / / / / /  / /_/ / /_/ / / / (__  ) 
-/_/ |_\___/|___/_/_/ /_/   \____/\__,_/_/ /_/____/";
+/_/ |_\___/|___/_/_/ /_/   \____/\__,_/_/ /_/____/"
 
-echo "NAME: KEVIN GANZ";
-echo "AGE: 14 YEARS OLD";
-echo "ADRESS: BATAM, INDONESIA";
-echo "PHONE: 081907406699";
-?>
+echo "NAME: KEVIN GANZ"
+echo "AGE: 14 YEARS OLD"
+echo "ADRESS: BATAM, INDONESIA"
+echo "PHONE: 081907406699"
